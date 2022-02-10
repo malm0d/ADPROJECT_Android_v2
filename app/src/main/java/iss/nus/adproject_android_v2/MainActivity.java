d0408984f028d945723a07b586bcb33dbdb2affc
smartSport <583535416@qq.com>
2022-02-10 15:53:30 +0800

    FirstPage

--- app/src/main/java/iss/nus/adproject_android_v2/MainActivity.java
+++ app/src/main/java/iss/nus/adproject_android_v2/MainActivity.java
@@ -2,13 +2,43 @@ package iss.nus.adproject_android_v2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button getBtn;
+    private Button pastmealBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        getBtn = findViewById(R.id.get);
+        getBtn.setOnClickListener(this);
+
+        pastmealBtn = findViewById(R.id.pastMeal);
+        pastmealBtn.setOnClickListener(this);
+    }
+
+
+
+    @Override
+    public void onClick(View v){
+
+        int id = v.getId();
+        if(id == R.id.get){
+
+
+        }else if (id == R.id.pastMeal){
+
+
+        }
+
+
     }
 }
